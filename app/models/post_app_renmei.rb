8751927f8b28d0cfe0fe2ceb9bc3d2dac55284bc
naoto hamada <hamada@vis-its.com>
2019-12-19 17:55:54 +0900

    Add post_app_renmei model

---
+++ app/models/post_app_renmei.rb
@@ -0,0 +1,18 @@
+class PostAppRenmei < ApplicationRecord
+  self.table_name = 'post_app_renmei'
+
+  belongs_to :post_app_adressbook, foreign_key: 'post_app_no'
+
+  scope :valid, -> { where.not(post_app_name_mei: '') }
+
+  def keisho
+    case post_app_renkeisho.to_i
+    when 1
+      '様'
+    when 5
+      '君'
+    when 6
+      'ちゃん'
+    end
+  end
+end
